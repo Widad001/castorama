30269b88b12f9a96f7f06a05ea32925dbd68d674
Widad001 <widad.chakra@hetic.net>
2022-07-21 15:30:00 +0100

    Ajouter les libraries necessaires

---
+++ dashboard/main.py
@@ -0,0 +1,14 @@
+## Libraries
+import streamlit as st
+# Lib system
+import sys
+import os
+from os.path import join, dirname
+from IPython.display import Image
+import pandas as pd
+# Lib data manipulation
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+from pathlib import Path
